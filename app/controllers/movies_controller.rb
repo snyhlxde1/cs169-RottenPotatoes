641bc66e64b3e84ed7c9b1f07c873c88e6b6c6ab
Lanxiang Hu <hlxde1@berkeley.edu>
2020-10-25 17:36:11 +0000

    part 1&2 complete attempt

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,10 +1,5 @@
 class MoviesController < ApplicationController
 
-  def initialize
-    @all_ratings = Movie.all_ratings
-    super
-  end
-  
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
